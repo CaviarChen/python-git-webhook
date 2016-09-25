c59a8a3bab807f5008f1cac7aeaded6e9e0da9d9
Caviar Chen <yzj@vlabpro.com>
2016-09-26 00:24:37 +1000

    bug fixed

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ import subprocess
 import json
 import os
 
+with open('config.json') as data_file:
+    config = json.load(data_file)
+log_path = os.path.abspath(config['server']['log_path']) + '/'
+
 def output_log(str):
-    f=open(config['server']['log_path']+"log.txt",'a')
+    f=open(log_path+"log.txt",'a')
     f.write(str+'\n')
     f.close()
     return
-
-
-with open('config.json') as data_file:
-    config = json.load(data_file)
 
 app = Flask(__name__)
 
@@ -26,12 +26,12 @@ def webhook(hook_name):
 
         if post_data["password"]!=config["webhooks"][hook_name]["key"]:
             abort(403)
-            
+
         time_str = strftime("%Y-%m-%d.%H.%M.%S", gmtime())
         output_log(time_str + "  " + hook_name)
-        detail_log_path = os.path.abspath(config['server']['log_path']+time_str+".txt")
+        detail_log_path = log_path+time_str+".txt"
         cmd_line = "cd " + config["webhooks"][hook_name]["local_path"]
-        cmd_line = cmd_line + "|git pull >"+detail_log_path;
+        cmd_line = cmd_line + " && git pull >"+detail_log_path;
 
         subprocess.Popen(cmd_line, shell=True)
     else:
